c4662cd3312d0bb0c89f4d6be2a0423cda79d11d
Shadsayine <Shadsayi.Thayalan001@student.weltec.ac.nz>
2022-09-14 13:08:12 +1200

    DELETE|EDIT|CREATE Fn Added

--- SampleSchoolWeb/Controllers/StudentController.cs
+++ SampleSchoolWeb/Controllers/StudentController.cs
@@ -17,5 +17,88 @@ namespace SampleSchoolWeb.Controllers
             IEnumerable<Student> objStudentList = _db.SchoolDB;
             return View(objStudentList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult Create(Student obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.SchoolDB.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+        //EDIT
+        //GET
+        public IActionResult Edit(int ? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var studentFromDb = _db.SchoolDB.Find(id);
+
+            if (studentFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(studentFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult Edit(Student obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.SchoolDB.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //DELETE
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var studentFromDb = _db.SchoolDB.Find(id);
+
+            if (studentFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(studentFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult DeletePOST(int ? id)
+        {
+            var obj = _db.SchoolDB.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.SchoolDB.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
